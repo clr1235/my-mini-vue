4e3f2dd68e9f8fdd68aa8b63982adf644d8f4c26
荣 <15035291867@163.com>
2022-12-05 16:24:24 +0800

    feat: 添加effect的stop的单测

--- src/tests/effect.spec.ts
+++ src/tests/effect.spec.ts
@@ -1,6 +1,6 @@
 // 响应式的本质核心就是 当变量的值改变的时候自动的去更新数据
 import {reactive} from '../reactive';
-import { effect } from '../effect';
+import { effect, stop } from '../effect';
 // describe 是一个将多个相关的测试组合到一起的块
 describe('effect', () => {
     it('happy path', () => {
@@ -35,5 +35,22 @@ describe('effect', () => {
         expect(foo).toBe(12);
         expect(res).toBe('foo')
     }); 
+
+    it('stop', () => {
+        let dummy
+        const obj = reactive({ prop: 1 })
+        const runner: any = effect(() => {
+          dummy = obj.prop
+        })
+        obj.prop = 2
+        expect(dummy).toBe(2)
+        stop(runner)
+        obj.prop = 3
+        expect(dummy).toBe(2)
+    
+        // stopped effect should still be manually callable
+        runner()
+        expect(dummy).toBe(3)
+    })
 
 }); 
